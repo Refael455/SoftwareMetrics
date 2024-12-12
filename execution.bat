b2f61c771d4ccfdfeac17909352637485d4b7d95
Refael <r.aharouniu@gmail.com>
2024-12-12 20:16:20 +0200

    Add execution

--- execution.bat
+++ execution.bat
@@ -1,4 +1,4 @@
-pip install -r C:\Users\rahar\Desktop\Test_exam\requirements.txt
+C:\Users\rahar\AppData\Local\Microsoft\WindowsApps\PythonSoftwareFoundation.Python.3.12_qbz5n2kfra8p0\python.exe -m pip install -r C:\Users\rahar\Desktop\Test_exam\requirements.txt
 C:\Users\rahar\AppData\Local\Microsoft\WindowsApps\PythonSoftwareFoundation.Python.3.12_qbz5n2kfra8p0\python.exe C:\Users\rahar\Desktop\Test_exam\test_average.py
 C:\Users\rahar\AppData\Local\Microsoft\WindowsApps\PythonSoftwareFoundation.Python.3.12_qbz5n2kfra8p0\python.exe -m coverage run -m pytest C:\Users\rahar\Desktop\Test_exam\test_average.py
 C:\Users\rahar\AppData\Local\Microsoft\WindowsApps\PythonSoftwareFoundation.Python.3.12_qbz5n2kfra8p0\python.exe -m coverage report -m >C:\Users\rahar\Desktop\Test_exam\report.txt
