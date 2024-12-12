c5ed354ad45b8de042b336eb5d24168d156552d3
Refael <r.aharouniu@gmail.com>
2024-12-12 20:04:55 +0200

    Add report and execution

--- execution.bat
+++ execution.bat
@@ -1,3 +1,4 @@
+pip install -r C:\Users\rahar\Desktop\Test_exam\requirements.txt
 python C:\Users\rahar\Desktop\Test_exam\test_average.py
 python -m coverage run -m pytest C:\Users\rahar\Desktop\Test_exam\test_average.py
 python -m coverage report -m >C:\Users\rahar\Desktop\Test_exam\report.txt
