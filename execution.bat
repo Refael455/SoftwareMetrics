2646036d337e895053fb9a9ed26a7e8e2290f2ca
Refael <r.aharouniu@gmail.com>
2024-12-12 20:00:50 +0200

    Add coverage and requirements

---
+++ execution.bat
@@ -0,0 +1,3 @@
+python C:\Users\rahar\Desktop\Test_exam\test_average.py
+python -m coverage run -m pytest C:\Users\rahar\Desktop\Test_exam\test_average.py
+python -m coverage report -m >C:\Users\rahar\Desktop\Test_exam\report.txt
